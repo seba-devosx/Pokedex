f6713366b8d1e0eae9606a37f483116db434c9d7
Sebastian Aravena sandoval <sebastianaravenasandoval@MacBook-Air-de-Sebastian.local>
2020-08-05 22:33:04 -0400

    se a rediseñado el index y esta casi completado solo falta ver que mas datos se deben mostrar por cada elemento

--- docs/js/BuscarPoke.js
+++ docs/js/BuscarPoke.js
@@ -38,20 +38,19 @@ function pokeStructure(data){
      
     
     const html=
-    `<div>
-    <div class="row justify-content-center">
-          <div id="pokemon-data" >
-          <div id="titulo"class="col-12 text-center">
-            <h4 id="pokedex-title" class="text-white">${name}</h4>
+    `  <div class="" id="pkemon" >
+    <div class="row justify-content-center" >
+      <div id="pokemon-data" style="background-color:${x} ">
+          <div id="titulo"class=" text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="2em" height="2em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M12 2a10 10 0 0 1 10 10a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2m0 2c-4.08 0-7.45 3.05-7.94 7h4.07c.44-1.73 2.01-3 3.87-3c1.86 0 3.43 1.27 3.87 3h4.07c-.49-3.95-3.86-7-7.94-7m0 16c4.08 0 7.45-3.05 7.94-7h-4.07c-.44 1.73-2.01 3-3.87 3c-1.86 0-3.43-1.27-3.87-3H4.06c.49 3.95 3.86 7 7.94 7m0-10a2 2 0 0 0-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2z" fill="white"/></svg>
+            <a id="pokedex-title" class="text-white">${name}</a>
           </div>
-          <div id="contenedor-img-poke" class="text-center"style="background-color:${x}">
-            <img id="imgpk" src=${img}>
-          </div>
-          <div id="pokemon-id" >
-            <h4 class="text-center text-capitalize text-white">#${id_poke}</h4>
-          </div>
-          <div id="pokemon-type"style="background-color:${x}" >
+          <div id="contenedor-img-poke" class="col-8 mt-2">
+            <div id="circulo-poke">
+              <img  src=${img}>
+            </div>
             <h4 class="text-center text-capitalize text-white">${typec}</h4>
+            <h6 class="text-center text-capitalize text-white">#${id_poke}</h6>
           </div>
           </div>
       </div>
